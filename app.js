1cabdbaaca0472757dbdff0d23d5545cab3bbe27
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-10-19 17:35:34 -0700

    [style.css] add styling and set up CSS variables

---
+++ app.js
@@ -0,0 +1,6 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    console.log(this.value);
+}
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
