e4a9c221d3847914be77227b59cac5611427b221
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-10-19 22:48:29 -0700

    [app.js] add event listeners to controls

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const inputs = document.querySelectorAll('.controls input');
 function handleUpdate() {
     console.log(this.value);
 }
+
+// listens on a certain spot
 inputs.forEach(input => input.addEventListener('change', handleUpdate))
+
+// listens on the mouse movement in the element
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
